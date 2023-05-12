258a405bda553977773e1071818756a337e105fb
James Peach <jpeach@apache.org>
2023-05-12 15:12:44 +1000

    Add a cdirip formula

---
+++ Formula/cdirip.rb
@@ -0,0 +1,13 @@
+class Cdirip < Formula
+  desc "cdirip is a program for extracting all the tracks contained in a CDI (DiscJuggler) image."
+  homepage "https://github.com/jozip/cdirip"
+  url "https://github.com/jozip/cdirip.git"
+  version "v0.6.4"
+  sha256 "5fd02b00b6bba10e801d2f57a8558820c2e4f833d806bb05269b48394e20648c"
+  license "GPL-2.0-or-later"
+
+  def install
+    system "make", "CC=#{ENV.cc}", "CFLAGS=-Os", "PREFIX=#{prefix}", "cdirip"
+    bin.install "cdirip" => "cdirip"
+  end
+end
